b2c971ced51d0a67c3d7828f3f739b0833f90f2d
freitas152 <freitas152@hotmail.com>
2022-08-07 16:53:18 -0300

    eventos

--- exercicio-15/eventos.js
+++ exercicio-15/eventos.js
@@ -1,17 +1,45 @@
 const button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
-    console.log('clicou no button');
+    const li = document.createElement('li');
+    li.textContent = 'Novo item';
+    // ul.append(li);
+    ul.prepend(li);
 })
+
+const ul = document.querySelector('ul');
+
+// ul.remove();
 
 const lis = document.querySelectorAll('li')
 
-lis.forEach((li)=>{
-    li.addEventListener('click',(event)=>{
-        // console.log(event.target);
-        // console.log(li);
-        const clickLi = event.target;
-        clickLi.style.textDecoration = 'line-through';
-    })
 
+
+// modo não muito eficiente.................................................
+
+// lis.forEach((li)=>{
+//     li.addEventListener('click',(event)=>{
+//         // console.log(event.target);
+//         // console.log(li);
+//         const clickLi = event.target;
+//         // clickLi.style.textDecoration = 'line-through';
+//         clickLi.remove();
+//     })
+
+// })
+
+
+
+ul.addEventListener('click', event => {
+    // console.log(event);
+    const clicado = event.target;
+    if (clicado.tagName === 'LI') {
+        clicado.remove();
+    }
 })
+
+
+
+
+
+
